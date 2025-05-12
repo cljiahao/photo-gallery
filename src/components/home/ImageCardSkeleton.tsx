124039ea2c45f3470b96ef0a05849223bedfaa3a
cljiahao <cljiahao@hotmail.com>
2025-05-13 01:23:31 +0800

    FEAT: correct the skeleton card size

--- src/components/home/ImageCardSkeleton.tsx
+++ src/components/home/ImageCardSkeleton.tsx
@@ -3,16 +3,16 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function ImageCardSkeleton() {
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-5">
-      <Skeleton className="h-[1000px] w-[1000px] rounded-xl" />
-      <Skeleton className="h-[1000px] w-[1000px] rounded-xl" />
-      <Skeleton className="h-[1000px] w-[1000px] rounded-xl" />
-      <Skeleton className="h-[1000px] w-[1000px] rounded-xl" />
-      <Skeleton className="h-[1000px] w-[1000px] rounded-xl" />
-      <Skeleton className="h-[1000px] w-[1000px] rounded-xl" />
-      <Skeleton className="h-[1000px] w-[1000px] rounded-xl" />
-      <Skeleton className="h-[1000px] w-[1000px] rounded-xl" />
-      <Skeleton className="h-[1000px] w-[1000px] rounded-xl" />
-      <Skeleton className="h-[1000px] w-[1000px] rounded-xl" />
+      <Skeleton className="h-[500px] w-[1000px] rounded-xl" />
+      <Skeleton className="h-[500px] w-[1000px] rounded-xl" />
+      <Skeleton className="h-[500px] w-[1000px] rounded-xl" />
+      <Skeleton className="h-[500px] w-[1000px] rounded-xl" />
+      <Skeleton className="h-[500px] w-[1000px] rounded-xl" />
+      <Skeleton className="h-[500px] w-[1000px] rounded-xl" />
+      <Skeleton className="h-[500px] w-[1000px] rounded-xl" />
+      <Skeleton className="h-[500px] w-[1000px] rounded-xl" />
+      <Skeleton className="h-[500px] w-[1000px] rounded-xl" />
+      <Skeleton className="h-[500px] w-[1000px] rounded-xl" />
     </div>
   );
 }
