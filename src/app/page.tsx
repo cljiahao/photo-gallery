efce3272716ffce6d895b9fe7342a8ec7704febe
cljiahao <cljiahao@hotmail.com>
2025-05-13 01:19:11 +0800

    FEAT: add spinner for suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,18 @@ import BackgroundImage from "@/components/home/BackgroundImage";
 import FilterBar from "@/components/home/FilterBar";
 import HeadLine from "@/components/home/HeadLine";
 import ImageList from "@/components/home/ImageList";
+import Spinner from "@/components/spinner";
 
 export default async function HomePage() {
   return (
     <div className="h-screen w-screen snap-y snap-mandatory overflow-y-auto">
       <header className="flex-center hw-full snap-center flex-col">
-        <Suspense>
+        <Suspense fallback={<Spinner />}>
           <BackgroundImage />
         </Suspense>
         <div className="flex-center flex-col rounded-xl bg-white p-12 opacity-85">
           <HeadLine />
-          <Suspense>
+          <Suspense fallback={<Spinner />}>
             <FilterBar />
           </Suspense>
         </div>
@@ -23,10 +24,10 @@ export default async function HomePage() {
         id="main"
         className="flex-center hw-full snap-start flex-col bg-black"
       >
-        <Suspense>
+        <Suspense fallback={<Spinner />}>
           <FilterBar />
         </Suspense>
-        <Suspense>
+        <Suspense fallback={<Spinner />}>
           <ImageList />
         </Suspense>
       </main>
